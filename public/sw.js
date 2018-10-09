a4928ec7a5c1eb322d58da46af39fe149103d3a2
svmi3195 <target1983@gmail.com>
2018-10-09 20:25:20 +0300

    open caches in service worker

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,14 @@
 //service worker
+let cacheName = 'showcaseCache001';
+
+self.addEventListener('install', function(event){
+  event.waitUntil(
+    caches.open(cacheName).then(function(cache){
+      return cache.addAll([
+        '/',
+        'index.html',
+        '/static/'
+      ])
+    })
+  )
+})
